4b80dc5eac53f0b27c25a59de78529beea665710
Artem <xan232@gmail.com>
2024-11-13 18:05:19 +0300

    Правильная постановка кубика

--- Assets/Minecraft/Scripts/InteractionSystemMinecraft.cs
+++ Assets/Minecraft/Scripts/InteractionSystemMinecraft.cs
@@ -24,15 +24,21 @@ public class InteractionSystemMinecraft : MonoBehaviour
         if (Input.GetButtonDown("Fire2") && Physics.Raycast(ray, out hit, 3f, LayerMask.GetMask("Cube")))
         {
             Destroy(hit.transform.gameObject);
+            
         }
-        if (Input.GetButtonDown("Fire1") && Physics.Raycast(ray, out hit, 3f))
+        if (Input.GetButtonDown("Fire1") && Physics.Raycast(ray, out hit, 3f, LayerMask.GetMask("Cube")))
         {
+            Vector3 position = hit.transform.position + (hit.normal * hit.transform.localScale.x);
             Vector3Int point = Vector3Int.zero;
-            point.x = Mathf.RoundToInt(hit.point.x);
-            point.y = Mathf.RoundToInt(hit.point.y);
-            point.z = Mathf.RoundToInt(hit.point.z);
-            GameObject cube = Instantiate(Cubes[0]);
-            cube.transform.position = point;
+            point.x = Mathf.RoundToInt(position.x);
+            point.y = Mathf.RoundToInt(position.y);
+            point.z = Mathf.RoundToInt(position.z);
+            Collider[] objects = Physics.OverlapBox(point, hit.transform.localScale * 0.45f);
+            if (objects.Length == 0)
+            {
+                GameObject cube = Instantiate(Cubes[0]);
+                cube.transform.position = point; 
+            }
 
         }
     }
